ecaf67c1c9a29198ee0164739afd557b8d34a2a6
Gracia Rizka Pasfica <igraciasriska1262@gmail.com>
2025-11-18 21:49:24 +0700

    Pertemuan 5 - Studi kasus Sistem Tarif Parkir

---
+++ Pertemuan 5/challenge/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func hitungParkir(jam int, isMember bool, isHoliday bool) int {
+	// Tarif dasar
+	total := 0
+
+	// 2 jam pertama = Rp 5000
+	if jam <= 2 {
+		total = 5000
+	} else {
+		// Jam ke-3 dan seterusnya Rp 2000/jam
+		total = 5000 + (jam-2)*2000
+	}
+
+	// Jika member
+	if isMember {
+		if jam <= 5 {
+			total = total - (total * 50 / 100) // diskon 50%
+		} else {
+			total = total - (total * 30 / 100) // diskon 30%
+		}
+	}
+
+	// Jika hari libur
+	if isHoliday {
+		total = total + 3000
+	}
+
+	return total
+}
+
+func main() {
+	// Contoh 1: 4 jam, bukan member, bukan hari libur
+	total1 := hitungParkir(4, false, false)
+	fmt.Println("Total biaya (4 jam, non-member, bukan libur): Rp", total1)
+
+	// Contoh 2: 2 jam, member, hari libur
+	total2 := hitungParkir(2, true, true)
+	fmt.Println("Total biaya (2 jam, member, hari libur): Rp", total2)
+}
